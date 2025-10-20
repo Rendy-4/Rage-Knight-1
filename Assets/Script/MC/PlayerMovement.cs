95259c27cf13473a2e7a49a4e1c5ad0a0f216b97
Rendy-4 <rendyjuans2@gmail.com>
2025-10-20 10:45:22 +0700

    percobaan pertama

---
+++ Assets/Script/MC/PlayerMovement.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float speed = 5f;
+    public Rigidbody2D rb;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        rb.linearVelocity = new Vector2(horizontal, vertical) * speed;
+    }
+}

4e4d33dec3e5f23afa1278fa72563b14c4c5b5ca
Rendy-4 <rendyjuans2@gmail.com>
2025-10-30 09:27:49 +0700

    memperbaiki bug enemy sama membuat animasinya

--- Assets/Script/Enemy/EnemyMovement.cs
+++ Assets/Script/Enemy/EnemyMovement.cs
@@ -1,40 +1,46 @@
-using System.Runtime.InteropServices;
 using UnityEngine;
-
 
 public class EnemyMovement : MonoBehaviour
 {
     private Rigidbody2D rb;
-    public Transform target;
+    private Transform target;
+    private Animator anim;
+
     public float speed = 3;
-    private bool isChasing;
-    private int facingDirection = 1;
+    private int facingDirection = -1;
+    private EnemyState AnimState, newState;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        ChangeState(EnemyState.Idle);
     }
 
     // Update is called once per frame
     void Update()
     {
-       if(isChasing == true)
+       if(AnimState == EnemyState.Chasing && target != null)
         {
+            if (target.position.x > transform.position.x && facingDirection == -1 || target.position.x < transform.position.x && facingDirection == 1)
+            {
+                Flip();
+            }
             UnityEngine.Vector2 direction = (target.position - transform.position).normalized;
-            rb.linearVelocity = direction * speed; // Mengganti linearVelocity dengan velocity
-        }
-
-        if (rb.linearVelocity.x > 0 && transform.localScale.x < 0 || rb.linearVelocity.x < 0 && transform.localScale.x > 0)
-        {
-            Flip();
+            rb.linearVelocity = direction * speed;
         }
     }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
-        {
-            isChasing = true;
+        {   
+            if (target == null) 
+            {
+                target = collision.transform;
+            }
+            ChangeState(EnemyState.Chasing);
         }
     }
 
@@ -42,14 +48,42 @@ public class EnemyMovement : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            isChasing = false;
-            rb.linearVelocity = UnityEngine.Vector2.zero; // Mengganti linearVelocity dengan velocity
+            rb.linearVelocity = UnityEngine.Vector2.zero;
         }
+        ChangeState(EnemyState.Idle);
     }
 
     void Flip()
     {
         facingDirection *= -1;
-        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+        transform.localScale = new UnityEngine.Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
     }
+    void ChangeState(EnemyState newState)
+    {
+        if (AnimState == EnemyState.Idle)
+        {
+            anim.SetBool("isIdle", false);
+        }
+        else if (AnimState == EnemyState.Chasing)
+        {
+            anim.SetBool("isChasing", false);
+        }
+
+        AnimState = newState;
+
+        if (AnimState == EnemyState.Idle)
+        {
+            anim.SetBool("isIdle", true);
+        }
+        else if (AnimState == EnemyState.Chasing)
+        {
+            anim.SetBool("isChasing", true);
+        }
+    }
+}
+
+public enum EnemyState
+{
+    Idle,
+    Chasing,
 }

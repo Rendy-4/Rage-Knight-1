8b3c57dd7f16c6050ad6a3b1027cd0d62c1deaae
Rendy-4 <rendyjuans2@gmail.com>
2025-11-03 10:18:30 +0700

    membuat prototype pause menu

---
+++ Assets/Script/PauseMenu.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class pauseMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseMenuUI;
+
+    public void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+    }
+    
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+    }
+    
+    
+}

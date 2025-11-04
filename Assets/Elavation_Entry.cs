e53a48f30e200f08ca0b996bfcefff50ef023a6b
Zeprhy <lianradith@gmail.com>
2025-11-04 21:12:39 +0700

    Mengatur Map #1

---
+++ Assets/Elavation_Entry.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class Elavation_Entry : MonoBehaviour
+{
+    public Collider2D[] mountainColliders;
+    public Collider2D[] boundaryColliders;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            foreach (Collider2D mountain in mountainColliders)
+            {
+                mountain.enabled = false;
+            }
+            foreach (Collider2D boundary in boundaryColliders)
+            {
+                boundary.enabled = true;
+            }
+            collision.gameObject.GetComponent<SpriteRenderer>().sortingOrder = 15;
+        }
+    }
+}

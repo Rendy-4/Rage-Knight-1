b54dc92665f2363078c5dc565bcd6299d9d05716
Rendy-4 <rendyjuans2@gmail.com>
2025-11-10 19:25:58 +0700

    memperbaiki system spawn

--- Assets/Script/Enemy/EnemySpawn.cs
+++ Assets/Script/Enemy/EnemySpawn.cs
@@ -1,37 +1,54 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class EnemySpawn : MonoBehaviour
 {
     public GameObject enemyPrefab;
     public float minSpawnTime = 2f;
     public float maxSpawnTime = 5f;
-    public int maxSpawn = 5;
-    public float timeUntilSpawn;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public int maxEnemies = 5;
+    public float RandomRangeX = 3f;
+    public float RandomRangeY = 3f;
+
+    private float timeUntilSpawn;
+    private List<GameObject> spawnedEnemies = new List<GameObject>();
+
     void Awake()
     {
         SetTimeUntilSpawn();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // Hapus musuh yang sudah mati (destroyed)
+        spawnedEnemies.RemoveAll(enemy => enemy == null);
+
         timeUntilSpawn -= Time.deltaTime;
-        if (timeUntilSpawn <= 0f && CountEnemies() < maxSpawn)
+        if (timeUntilSpawn <= 0f && spawnedEnemies.Count < maxEnemies)
         {
-            Vector3 randPos = transform.position + (Vector3)Random.insideUnitCircle * 3f;
-            Instantiate(enemyPrefab, randPos, Quaternion.identity);
+            float randomX = Random.Range(-RandomRangeX, RandomRangeX);
+            float randomY = Random.Range(-RandomRangeY, RandomRangeY);
+
+            Vector3 randPos = new Vector3(
+                transform.position.x + randomX,
+                transform.position.y + randomY,
+                0f);
+
+            GameObject newEnemy = Instantiate(enemyPrefab, randPos, Quaternion.identity);
+            spawnedEnemies.Add(newEnemy);
+
             SetTimeUntilSpawn();
         }
     }
 
-    public void SetTimeUntilSpawn()
+    void SetTimeUntilSpawn()
     {
         timeUntilSpawn = Random.Range(minSpawnTime, maxSpawnTime);
     }
 
-    int CountEnemies()
+    void OnDrawGizmosSelected()
     {
-        return GameObject.FindGameObjectsWithTag("Enemy").Length;
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireCube(transform.position, new Vector3(RandomRangeX * 2, RandomRangeY * 2, 0f));
     }
 }

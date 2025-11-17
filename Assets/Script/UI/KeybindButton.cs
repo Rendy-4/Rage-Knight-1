a858365307f6946a2d90a3f9b9fa0d9b175d2af2
Zeprhy <lianradith@gmail.com>
2025-11-17 23:38:31 +0700

    Fix Bug UI Rebind

---
+++ Assets/Script/UI/KeybindButton.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using TMPro;
+
+public class KeybindButton : MonoBehaviour
+{
+    public string actionName;              
+    public TextMeshProUGUI keytext;        
+
+    private bool waitingForInput = false;
+
+    private void Start()
+    {
+        keytext.text = KeybindManager.Instance.GetKey(actionName).ToString();
+    }
+
+    public void OnClickButton()
+    {
+        waitingForInput = true;
+        keytext.text = "Press any key...";
+    }
+
+    private void Update()
+    {
+        if (!waitingForInput)
+            return;
+
+        if (Input.anyKeyDown)
+        {
+            KeyCode newKey = DetectKey();
+
+            if (newKey == KeyCode.None)
+                return;
+
+            if (KeybindManager.Instance.IsKeyAlreadyUsed(newKey, actionName))
+            {
+                // jika ingin popup → tinggal panggil popup.Show();
+                Debug.Log("Key sudah dipakai!");
+                keytext.text = KeybindManager.Instance.GetKey(actionName).ToString();
+                waitingForInput = false;
+                return;
+            }
+
+            KeybindManager.Instance.SetKey(actionName, newKey);
+            keytext.text = newKey.ToString();
+            waitingForInput = false;
+        }
+    }
+
+    private KeyCode DetectKey()
+    {
+        foreach (KeyCode key in System.Enum.GetValues(typeof(KeyCode)))
+        {
+            if (Input.GetKeyDown(key))
+                return key;
+        }
+        return KeyCode.None;
+    }
+}
